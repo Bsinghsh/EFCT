7f702e341e178203777f423f193bfe6c05736fca
Bsinghsh <bsinghsh@purdue.edu>
2019-11-25 17:04:29 -0800

    Fixed some minor CSS issues

--- client/src/App/pages/List.js
+++ client/src/App/pages/List.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 
 class List extends Component {
   // Initialize the state
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       list: []
@@ -17,35 +19,41 @@ class List extends Component {
   // Retrieves the list of items from the Express app
   getList = () => {
     fetch('/api/getList')
-    .then(res => res.json())
-    .then(list => this.setState({ list }))
+      .then(res => res.json())
+      .then(list => this.setState({
+        list
+      }))
   }
 
   render() {
-    const { list } = this.state;
+    const {
+      list
+    } = this.state;
 
-    return (
-      <div className="App">
-        <h1>List of Items</h1>
-        {/* Check to see if any items are found*/}
-        {list.length ? (
-          <div>
-            {/* Render the list of items */}
-            {list.map((item) => {
-              return(
-                <div>
-                  {item}
-                </div>
+    return ( <
+      div className = "App" >
+      <
+      h1 > List of Items < /h1> { /* Check to see if any items are found*/ } {
+        list.length ? ( <
+          div > { /* Render the list of items */ } {
+            list.map((item) => {
+              return ( <
+                div > {
+                  item
+                } <
+                /div>
               );
-            })}
-          </div>
-        ) : (
-          <div>
-            <h2>No List Items Found</h2>
-          </div>
+            })
+          } <
+          /div>
+        ) : ( <
+          div >
+          <
+          h2 > No List Items Found < /h2> <
+          /div>
         )
-      }
-      </div>
+      } <
+      /div>
     );
   }
 }
